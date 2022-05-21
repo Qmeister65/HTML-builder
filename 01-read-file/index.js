3dba8eabbd7a0d090685be47abe643d5989dadf7
Aleksandr Semenov <rexsanya1999@gmail.com>
2022-05-21 14:46:53 +0300

    fix: fix task 1 solution

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,5 @@
 const path = require('path');
 const fs = require('fs');
 
-fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+fs.createReadStream(path.join(__dirname, './text.txt')).on('data', data => console.log(data.toString()));
 
